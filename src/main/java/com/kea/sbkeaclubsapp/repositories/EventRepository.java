bf82b1a09bc48ff647fc4017ffa2ed8696bbaa6c
elleom <el_leo_m@hotmail.com>
2021-12-11 20:08:49 +0100

    added repos

--- src/main/java/com/kea/sbkeaclubsapp/repositories/EventRepository.java
+++ src/main/java/com/kea/sbkeaclubsapp/repositories/EventRepository.java
@@ -1,14 +1,12 @@
 package com.kea.sbkeaclubsapp.repositories;
 
 import com.kea.sbkeaclubsapp.model.Event;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 /**
  * @author el_le
  * @since 11/12/2021 16:35
  */
 
-public interface EventRepository extends ReactiveCrudRepository<Event, Long> {
+public interface EventRepository extends CrudRepository<Event, Long> {
 }
