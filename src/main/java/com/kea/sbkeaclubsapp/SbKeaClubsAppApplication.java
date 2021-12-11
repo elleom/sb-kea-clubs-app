88246240dd5b0115ba5e40dd101fef83bc548af3
elleom <el_leo_m@hotmail.com>
2021-12-11 16:18:41 +0100

    Initial commit

---
+++ src/main/java/com/kea/sbkeaclubsapp/SbKeaClubsAppApplication.java
@@ -0,0 +1,13 @@
+package com.kea.sbkeaclubsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbKeaClubsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbKeaClubsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kea/sbkeaclubsapp/SbKeaClubsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.sbkeaclubsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKeaClubsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
