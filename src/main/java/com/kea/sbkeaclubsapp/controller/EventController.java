92b0191bce317ae13401c6d0ab21acc5d40ea17c
elleom <el_leo_m@hotmail.com>
2021-12-12 21:44:53 +0100

    added swagger

--- src/main/java/com/kea/sbkeaclubsapp/controller/EventController.java
+++ src/main/java/com/kea/sbkeaclubsapp/controller/EventController.java
@@ -1,25 +1,39 @@
 package com.kea.sbkeaclubsapp.controller;
 
 import com.kea.sbkeaclubsapp.model.Event;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kea.sbkeaclubsapp.repositories.EventRepository;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.*;
 
 /**
  * @author el_le
  * @since 11/12/2021 10:58
  */
 
-@RequestMapping("/api/event/")
+@RequestMapping("/api/events/")
 @RestController
 public class EventController {
 
+    private final EventRepository eventRepository;
+
+    public EventController(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
     @GetMapping("{eventId}")
     public Event getSingleEvent(@PathVariable Long eventId){
-        Event event = Event.builder().id(1L).description("An event").build();
-        return event;
+        return eventRepository.findById(eventId).get();
+    }
+
+    @GetMapping
+    public List<Event> getAllEvent(){
+        List<Event> eventsList = new ArrayList<>();
+        eventRepository.findAll().forEach(eventsList::add);
+        return eventsList;
     }
 }
